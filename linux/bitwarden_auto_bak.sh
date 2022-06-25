afbf53a1ca16c827eb572767bfc2b1ee1a6ae317
honbey <git@honbey.com>
2022-06-25 18:49:15 +0800

    perf(bitwarden): update backup config

--- linux/bitwarden_auto_bak.sh
+++ linux/bitwarden_auto_bak.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# 此脚本用于自动备份 BitWarden 的文件
+# 此脚本用于自动备份 Vaultwarden(Bitwarden) 的文件
 
 # crontab -e
 # 0 0 15 * * /path/bitwarden_auto_bak.sh $1
 
 set -e
 
-FILE_NAME=bitwarden_bak_$(date +"%Y-%m-%d").tar.gz
+FILE_NAME="vaultwarden_bak_$(date +"%Y-%m-%d").tar.gz"
 
-file_path=${1}
+file_path="${1}"
 cd "${file_path}/data/"
 tar czf "${FILE_NAME}" ./*
 mv "${FILE_NAME}" "${file_path}/backup/"
