eb62cd345ebe290a983aff788615982c04dffcca
honbey <git@honbey.com>
2023-01-05 22:11:11 +0800

    perf(python): change upgrade strategy

--- linux/upgrade_python_packages.sh
+++ linux/upgrade_python_packages.sh
@@ -2,5 +2,5 @@
 # 
 
 pip list --outdated | sed -n '3,$p' | awk '{print $1}' > upgrade.txt
-cat < upgrade.txt | xargs pip install --upgrade
+cat < upgrade.txt | xargs pip install --upgrade-strategy only-if-needed
 rm upgrade.txt
