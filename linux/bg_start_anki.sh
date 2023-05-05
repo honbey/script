6df513bcf95e8bfbcd6bbf39aaab79c67f2863fe
honbey <git@honbey.com>
2023-05-05 20:11:54 +0800

    perf(fava & anki): update

--- linux/bg_start_anki.sh
+++ linux/bg_start_anki.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-SYNC_USER1=${1}:${2}
-SYNC_BASE=/opt/data/server/anki
-SYNC_HOST=127.0.0.1
-SYNC_PORT=1050
+rm -rf /tmp/tmp*-anki.pid
 
-nohup /opt/data/pyvenv/anki/bin/python -m anki.syncserver > /opt/data/log/anki.log 2>&1 &
+SYNC_USER1=zhang:honbey SYNC_BASE=/opt/data/server/anki SYNC_HOST=127.0.0.1 SYNC_PORT=1050 nohup /opt/data/pyvenv/anki/bin/python -m anki.syncserver > /opt/data/log/anki.log 2>&1 &
 
-TMPDIR="$(mktemp)-anki.pid"
-echo $! > "${TMPDIR}"
+TMPDIR="$(mktemp)"
+echo $! > "${TMPDIR}-anki.pid"
+rm -rf ${TMPDIR}
--- linux/bg_start_fava.sh
+++ linux/bg_start_fava.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+rm -rf /tmp/tmp.*-fava.pid
+
 nohup /opt/data/pyvenv/ledger/bin/fava /opt/data/server/ledger/main.bean > /dev/null 2>&1
 &
 
-TMPDIR="$(mktemp)-fava.pid"
-echo $! > "${TMPDIR}"
+TMPDIR="$(mktemp)"
+echo $! > "${TMPDIR}-fava.pid"
+rm -rf ${TMPDIR}
