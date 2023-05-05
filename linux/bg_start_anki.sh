e8eadc4a657ceaf529c77efddcf4466728e3b953
honbey <git@honbey.com>
2023-05-05 20:13:59 +0800

    perf(anki): update

--- linux/bg_start_anki.sh
+++ linux/bg_start_anki.sh
@@ -2,7 +2,7 @@
 
 rm -rf /tmp/tmp*-anki.pid
 
-SYNC_USER1=zhang:honbey SYNC_BASE=/opt/data/server/anki SYNC_HOST=127.0.0.1 SYNC_PORT=1050 nohup /opt/data/pyvenv/anki/bin/python -m anki.syncserver > /opt/data/log/anki.log 2>&1 &
+SYNC_USER1="${1}:${2}" SYNC_BASE=/opt/data/server/anki SYNC_HOST=127.0.0.1 SYNC_PORT=1050 nohup /opt/data/pyvenv/anki/bin/python -m anki.syncserver > /opt/data/log/anki.log 2>&1 &
 
 TMPDIR="$(mktemp)"
 echo $! > "${TMPDIR}-anki.pid"
