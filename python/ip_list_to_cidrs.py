267f600373ae7ea317f1684e731b5fc45ccab1f4
honbey <git@honbey.com>
2024-03-06 02:21:18 +0800

    style: format codes

--- python/ip_list_to_cidrs.py
+++ python/ip_list_to_cidrs.py
@@ -1,7 +1,7 @@
 import ipaddress
 
 ip_list = []
-with open('ip.txt', 'r', encoding='utf-8') as f:
+with open("ip.txt", "r", encoding="utf-8") as f:
     line = f.readline()
     while line:
         ip_list.append(line.strip())
@@ -11,4 +11,3 @@ nets = [ipaddress.ip_network(_ip) for _ip in ip_list]
 cidrs = ipaddress.collapse_addresses(nets)
 for k, v in enumerate(cidrs):
     print(v)
-
